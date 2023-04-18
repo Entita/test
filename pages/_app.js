88908ceb16cdf3c8ef73ce7bad7546b10900de45
Jakub Hampl <entitak@gmail.com>
2023-04-18 17:06:37 +0200

    a

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,8 @@ import { GlobalStyle } from '../styles/global.style'
 import Notification from '../components/Notification'
 import axios from 'axios'
 
-if (process.env.NODE_ENV == 'development') {
-  axios.defaults.baseURL = 'http://localhost:3000/api/'
-} else {
-  axios.defaults.baseURL = 'https://test-entita.vercel.app/api/'
-}
+if (process.env.NODE_ENV == 'development') axios.defaults.baseURL = 'http://localhost:3000/api/'
+else axios.defaults.baseURL = 'https://test-entita.vercel.app/api/'
 
 // basic component of react and nextjs
 function MyApp({ Component, pageProps }) {
